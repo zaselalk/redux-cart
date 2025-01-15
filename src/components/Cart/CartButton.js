102948cb0c43e69661399d9dce4b4ca645211be7
zaselalk <43272744+zaselalk@users.noreply.github.com>
2025-01-15 20:50:53 +0530

    :bug: Update CartButton to display the correct number of items in the cart

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,9 +1,10 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import classes from "./CartButton.module.css";
 import { setCartShowing } from "../../store/slices/cart.state";
 
 const CartButton = (props) => {
   const dispatch = useDispatch();
+  const cartItems = useSelector((state) => state.cart.items);
 
   const toggleCartHandler = () => {
     dispatch(setCartShowing());
@@ -11,7 +12,7 @@ const CartButton = (props) => {
   return (
     <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartItems.length}</span>
     </button>
   );
 };
