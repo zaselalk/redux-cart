3b74d8464f4780e858d8d4af28abf50f867e5e81
zaselalk <43272744+zaselalk@users.noreply.github.com>
2025-01-15 20:48:54 +0530

    :sparkles: Implement cart functionality with Redux integration and total amount calculation

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,5 +1,5 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
 
 const Products = (props) => {
   return (
@@ -7,9 +7,14 @@ const Products = (props) => {
       <h2>Buy your favorite products</h2>
       <ul>
         <ProductItem
-          title='Test'
+          title="Test"
           price={6}
-          description='This is a first product - amazing!'
+          description="This is a first product - amazing!"
+        />
+        <ProductItem
+          title="Test 2"
+          price={6}
+          description="This is a first product - amazing!"
         />
       </ul>
     </section>
--- src/store/slices/cart.state.js
+++ src/store/slices/cart.state.js
@@ -4,11 +4,30 @@ const cartSlice = createSlice({
   name: "cart",
   initialState: {
     items: [],
+    totalAmount: 0,
     isShowing: false,
   },
   reducers: {
     addItem(state, action) {
-      state.items.push(action.payload);
+      if (state.items.length > 0) {
+        const existingItem = state.items.find(
+          (item) => item.title === action.payload.title
+        );
+        if (existingItem) {
+          existingItem.quantity++;
+          existingItem.total = existingItem.price * existingItem.quantity;
+          state.totalAmount += existingItem.price;
+          return;
+        }
+      }
+      state.items.push({
+        id: Math.random().toString(),
+        title: action.payload.title,
+        price: action.payload.price,
+        quantity: 1,
+        total: action.payload.price,
+      });
+      state.totalAmount += action.payload.price;
     },
     removeItem(state, action) {
       state.items = state.items.filter((item) => item.id !== action.payload.id);
